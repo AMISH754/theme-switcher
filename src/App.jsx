7b991be9022ad9eed118e67ce41c840eecbe98ee
Salil Lakra <salillakra.dev@gmail.com>
2025-07-19 15:22:39 +0530

    fix: dark mode toggle not working.

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,40 @@
-import { useState } from 'react'
-import ThemeBtn from "./components/ThemeBtn"
-import Card from "./components/Card"
-import {ThemeProvider} from './context/theme'
- import {useEffect} from 'react'
+import { useState } from "react";
+import ThemeBtn from "./components/ThemeBtn";
+import Card from "./components/Card";
+import { ThemeProvider } from "./context/theme";
+import { useEffect } from "react";
 function App() {
-const [themeMode,setThemeMode]=useState('light');
-const darkTheme=()=>{
-    setThemeMode('dark');
-}
-const lightTheme=()=>{
-    setThemeMode('light');
-}
-useEffect(()=>{
-document.querySelector('html').classList.remove('dark',"light") ;   
-document.querySelector('html').classList.add(themeMode);
-},[themeMode]);
+  const [themeMode, setThemeMode] = useState("light");
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+  useEffect(() => {
+    const html = document.querySelector("html");
+    if (html.hasAttribute("data-theme")) {
+      html.setAttribute("data-theme", themeMode);
+    } else {
+      html.setAttribute("data-theme", themeMode);
+    }
+  }, [themeMode]);
 
   return (
-   <ThemeProvider value={{themeMode, darkTheme,lightTheme}}>
-  <div className="flex flex-wrap min-h-screen items-center">
-          <div className="w-full">
-              <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-                  <ThemeBtn />
-              </div>
-
-              <div className="w-full max-w-sm mx-auto">
-                  <Card />
-              </div>
+    <ThemeProvider value={{ themeMode, darkTheme, lightTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn />
           </div>
-      </div>
-      </ThemeProvider> 
 
-  )
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
